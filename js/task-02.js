973977edad9bc5cda612fd17d5767f9e82901e23
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-11 00:51:22 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+console.log(list);
+
+const items = ingredients.forEach(ingredient => {
+  let li = document.createElement('li');
+li.classList.add('item');
+li.textContent = ingredient;
+list.append(li);
+})
