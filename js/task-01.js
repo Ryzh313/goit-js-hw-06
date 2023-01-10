ac501c0d30b42d1459c2517bbe37eeb534fe0d78
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-10 17:01:54 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
-const article = document.querySelector(".article");
+// 1.1 Task
+const itemEl = document.querySelectorAll(".item");
+const itemElAmount = itemEl.length;
+console.log('Number of categories: ',itemElAmount);
+// 1.2 Task
+itemEl.forEach(item => {
+    console.log('Category: ', item.firstElementChild.textContent);
+console.log('Elements: ', item.lastElementChild.children.length)
+})
