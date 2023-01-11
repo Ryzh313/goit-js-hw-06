9219d427a8f9eb8d1a2457bf513a73cce8f49dd3
Ryzhenko Oleksii <116575138+Ryzh313@users.noreply.github.com>
2023-01-11 02:27:27 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,52 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// variant 1 = .map
+
+
+const imgList = document.querySelector('.gallery');
+console.log(imgList);
+
+const createImageGallery = ({ url, alt }) => {
+  return `<li><img src="${url}" alt="${alt}" width = '1260' height = '750'></li>`;
+};
+console.log(createImageGallery);
+
+const createImages = images.map(createImageGallery).join('');
+console.log(createImages);
+
+imgList.insertAdjacentHTML("beforeend", createImages);
+
+imgList.style.display = 'flex';
+imgList.style.justifyContent = 'center';
+imgList.style.flexWrap = 'wrap';
+imgList.style.gap = '20px';
+imgList.style.listStyle = 'none';
+
+
+// variant 2 = .reduce
+
+
+const imgList2 = document.querySelector('.gallery');
+console.log(imgList);
+
+const createImageGallery2 = ({ url, alt }) => {
+  return `<li><img src="${url}" alt="${alt}" width = '1260' height = '750'></li>`;
+};
+console.log(createImageGallery2);
+
+const createImages2 = images.reduce(
+  (accaccumulator, image) => accaccumulator + createImageGallery2(image),
+  ""
+);
+
+imgList2.insertAdjacentHTML("beforeend", createImages2);
+
+imgList.style.display = 'flex';
+imgList.style.justifyContent = 'center';
+imgList.style.flexWrap = 'wrap';
+imgList.style.gap = '20px';
+imgList.style.listStyle = 'none';
+
+
